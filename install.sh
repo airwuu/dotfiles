a1cac9db39e78dade7ef23b5ece4da6aa462b280
Jess <toxocious@gmail.com>
2025-05-31 09:03:38 -0700

    feat: backup nixos hardware configuration during installation script exec

--- install.sh
+++ install.sh
@@ -7,6 +7,12 @@ mkdir -p ~/Videos
 echo " - Core directories have been created"
 echo ""
 
+# Backing up nixos hardware configuration.
+echo "Backing up your NixOS Hardware Configuration"
+cp /etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix.bak
+echo " - Backup complete (/etc/nixos/hardware-configuration.nix.bak)"
+echo ""
+
 # Copy the dotfiles and nix configuration files into .config/nixos.
 echo "Copying dotfiles into ${HOME}/.config/nixos"
 mkdir -p ~/.config/nixos

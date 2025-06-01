6863d8529d277e94b29a4b69ab7db1d4938e2172
Jess <toxocious@gmail.com>
2025-05-31 23:00:51 -0700

    fix: home manager program set in host/home.nix files

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -1,58 +1,58 @@
 { pkgs, config, ... }:
 {
-  imports = [
-    # User Specific Config
-    ./variables.nix
+    imports = [
+        # User Specific Config
+        ./variables.nix
 
-    # Programs
-    ../../home/programs/discord
-    ../../home/programs/git
-    ../../home/programs/ghostty
+        # Programs
+        ../../home/programs/discord
+        ../../home/programs/git
+        ../../home/programs/ghostty
 
-    # Scripts
+        # Scripts
 
-    # System
-    ../../home/system/hyprpaper
-    ../../home/system/hyprland
-    ../../home/system/hyprpanel
-    ../../home/system/hypridle
-    ../../home/system/hyprlock
-    ../../home/system/cursor
-    ../../home/system/launcher
-  ];
-
-  home = {
-    # Set the home directory path for our user
-    inherit (config.var) username;
-    homeDirectory = "/home/" + config.var.username;
-
-    # Packages
-    packages = with pkgs; [
-      # Apps
-      brave
-
-      # Development - Languages
-      nodejs
-      python3
-
-      # Development - IDE
-      vscode
-
-      # Utilities
-      zip
-      unzip
-      tree
-      btop
-      neofetch
+        # System
+        ../../home/system/hyprpaper
+        ../../home/system/hyprland
+        ../../home/system/hyprpanel
+        ../../home/system/hypridle
+        ../../home/system/hyprlock
+        ../../home/system/cursor
+        ../../home/system/launcher
     ];
+
+    home = {
+        # Set the home directory path for our user
+        inherit (config.var) username;
+        homeDirectory = "/home/" + config.var.username;
+
+        # Packages
+        packages = with pkgs; [
+        # Apps
+        brave
+
+        # Development - Languages
+        nodejs
+        python3
+
+        # Development - IDE
+        vscode
+
+        # Utilities
+        zip
+        unzip
+        tree
+        btop
+        neofetch
+        ];
+
+        # State version - don't touch this
+        stateVersion = "24.11";
+    };
 
     # Enable Home Manager
     programs.home-manager = {
         enable = true;
         nixVersions.stable = true;
     };
-
-     # State version - don't touch this
-     stateVersion = "24.11";
-  };
 }

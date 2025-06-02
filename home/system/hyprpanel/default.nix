29562a0f2101865bf418962e10e1c4ce97f172c5
Jess <toxocious@gmail.com>
2025-06-02 11:19:05 -0700

    fix: streamline hyprpanel configuration by removing commented-out settings

--- home/system/hyprpanel/default.nix
+++ home/system/hyprpanel/default.nix
@@ -14,76 +14,22 @@
         # };
 
         # # General settings
-        # settings = {
-        #     theme = {
-        #         font.size = "12";
+        settings = {
+            layout = {
+                "bar" = {
+                    "scaling" = 1.0;
+                    "monitor" = "DP-1";
+                };
 
-        #         bar = {
-        #             transparent = true;
-        #             floating = true;
-
-        #             location = "top";
-        #             buttons.padding_x = "0.8rem";
-        #             buttons.padding_y = "0.4rem";
-
-        #             buttons.monochrome = true;
-        #             menus.monochrome = true;
-        #         };
-        #     };
-
-        #     bar = {
-        #         workspaces.show_numbered = false;
-        #         workspaces.workspaces = 9;
-        #         workspaces.numbered_active_indicator = "color";
-        #         workspaces.monitorSpecific = false;
-        #         workspaces.showApplicationIcons = true;
-        #         workspaces.showWsIcons = true;
-
-        #         # clock = {
-        #         #     format = "%b %d %I:%M %p";
-        #         #     showIcon = false;
-        #         # };
-
-        #         windowtitle = {
-        #             label = true;
-        #         };
-        #         volume = {
-        #             label = false;
-        #         };
-        #         network = {
-        #             truncation_size = 12;
-        #         };
-        #         bluetooth = {
-        #             label = false;
-        #         };
-        #         notifications = {
-        #             show_total = true;
-        #         };
-        #         media = {
-        #             show_active_only = true;
-        #         };
-        #     };
-
-        #     notifications = {
-        #         position = "top right";
-        #         showActionsOnHover = true;
-        #     };
-
-        #     layout = {
-        #         "bar" = {
-        #             "scaling" = 1.0;
-        #             "monitor" = "DP-1";
-        #         };
-
-        #         "bar.layouts" = {
-        #             "*" = {
-        #                 "left" = [ "dashboard" "workspaces" "windowtitle" ];
-        #                 "middle" = [ "media" ];
-        #                 "right" = [ "volume" "network" "clock" "systray" "notifications" ];
-        #             };
-        #         };
-        #     };
-        # };
+                "bar.layouts" = {
+                    "*" = {
+                        "left" = [ "dashboard" "workspaces" "windowtitle" ];
+                        "middle" = [ "media" ];
+                        "right" = [ "volume" "network" "clock" "systray" "notifications" ];
+                    };
+                };
+            };
+        };
     };
 
     home.file = {
@@ -116,14 +62,6 @@
                 "bar.notifications.hideCountWhenZero": true,
                 "notifications.position": "top right",
                 "notifications.showActionsOnHover": true,
-
-                "bar.scaling": 1.0,
-                "bar.monitor": "DP-1",
-                "bar.layouts.*": {
-                    "left": ["dashboard", "workspaces", "windowtitle"],
-                    "middle": ["media"],
-                    "right": ["volume", "network", "clock", "systray", "notifications"]
-                },
 
                 "theme.font.size": "12",
                 "theme.bar.transparent": true,

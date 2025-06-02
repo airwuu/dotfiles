a97d697430ed30b80cfd3c3325af8a72f4c43ba7
Jess <toxocious@gmail.com>
2025-06-02 10:48:23 -0700

    chore: misc tweaks to hyprpanel config

--- home/system/hyprpanel/default.nix
+++ home/system/hyprpanel/default.nix
@@ -92,6 +92,38 @@
                 "bar.clock.format": "%b %d %I:%M %p",
                 "bar.clock.showIcon": false,
 
+                "bar.workspaces.show_numbered": false,
+                "bar.workspaces.showAllActive": false,
+                "bar.workspaces.workspaces": 9,
+                "bar.workspaces.numbered_active_indicator": "color",
+                "bar.workspaces.monitorSpecific": false,
+                "bar.workspaces.showApplicationIcons": true,
+                "bar.workspaces.showWsIcons": true,
+
+                "bar.windowtitle.label": true,
+
+                "bar.volume.label": false,
+
+                "bar.network.label": false,
+                "bar.network.truncation_size": 12,
+
+                "bar.bluetooth.label": false,
+
+                "bar.media.show_active_only": true,
+
+                "bar.notifications.show_total": true,
+                "bar.notifications.hideCountWhenZero": true,
+                "notifications.position": "top right",
+                "notifications.showActionsOnHover": true,
+
+                "bar.scaling": 1.0,
+                "bar.monitor": "DP-1",
+                "bar.layouts.*": {
+                    "left": ["dashboard", "workspaces", "windowtitle"],
+                    "middle": ["media"],
+                    "right": ["volume", "network", "clock", "systray", "notifications"]
+                },
+
                 "theme.font.size": "12",
                 "theme.bar.transparent": true,
                 "theme.bar.floating": true,
@@ -100,31 +132,6 @@
                 "theme.bar.buttons.padding_y": "0.4rem",
                 "theme.bar.buttons.monochrome": true,
                 "theme.bar.menus.monochrome": true,
-
-                "bar.workspaces.show_numbered": false,
-                "bar.workspaces.workspaces": 9,
-                "bar.workspaces.numbered_active_indicator": "color",
-                "bar.workspaces.monitorSpecific": false,
-                "bar.workspaces.showApplicationIcons": true,
-                "bar.workspaces.showWsIcons": true,
-
-                "bar.windowtitle.label": true,
-                "bar.volume.label": false,
-                "bar.network.truncation_size": 12,
-                "bar.bluetooth.label": false,
-                "bar.notifications.show_total": true,
-                "bar.media.show_active_only": true,
-
-                "notifications.position": "top right",
-                "notifications.showActionsOnHover": true,
-
-                "layout.bar.scaling": 1.0,
-                "layout.bar.monitor": "DP-1",
-                "layout.bar.layouts.*": {
-                    "left": ["dashboard", "workspaces", "windowtitle"],
-                    "middle": ["media"],
-                    "right": ["volume", "network", "clock", "systray", "notifications"]
-                }
             }
         '';
     };

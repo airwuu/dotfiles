a4283b22e922a578cd6c1f0bd43640e18e380733
toxocious <toxocious@gmail.com>
2025-06-13 00:01:57 -0700

    fix: setting spicetify theme

--- home/programs/spicetify/default.nix
+++ home/programs/spicetify/default.nix
@@ -8,21 +8,8 @@
     {
         enable = true;
 
-        theme = {
-            name = "hazy";
-            src = pkgs.fetchgit {
-                url = "https://github.com/spicetify/spicetify-themes";
-                rev = "0893ed8938f1fdc05d34e5c989941358f479ad84";
-                sha256 = "04ajrxn539ygrkfp4wz0pkjf229058wbrgjd1gwv0arspflp7yb6";
-            };
-
-            injectCss = true;
-            injectThemeJs = true;
-            replaceColors = true;
-            homeConfig = true;
-            overwriteAssets = false;
-            additonalCss = "";
-        };
+        theme = spicePkgs.themes.dribbblish;
+        colorScheme = "catppuccin-macchiato";
 
         enabledExtensions = with spicePkgs.extensions; [
             adblockify

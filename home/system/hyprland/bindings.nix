9cc472d84eb4252e6a834fee2e5a0d36d2e9bebc
airwuu <awu0103@gmail.com>
2025-08-17 19:21:15 -0700

    feat: add dragging windows

--- home/system/hyprland/bindings.nix
+++ home/system/hyprland/bindings.nix
@@ -21,6 +21,18 @@ in
       "$mod, RETURN, exec, caelestia-quickshell shell toggle launcher"
       "$mod, D, exec, caelestia-quickshell shell toggle dashboard"
       "$mod, S, exec, caelestia-quickshell shell toggle session"
+
+      # Volume and Media Keys
+      ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+      ", XF86AudioPlay, exec, playerctl play-pause"
+      ", XF86AudioNext, exec, playerctl next"
+      ", XF86AudioPrev, exec, playerctl previous"
+
+      # Brightness Keys
+      ", XF86MonBrightnessUp, exec, brightnessctl set +5%"
+      ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
     ] ++ ( builtins.concatLists(builtins.genList (i:
       let ws = i + 1;
       in [
@@ -28,5 +40,9 @@ in
         "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
       ]) totalWorkspaces
     ));
+    bindm = [
+      # Move window with mainMod + LMB
+      "$mod, mouse:272, movewindow"
+    ];
   };
 }

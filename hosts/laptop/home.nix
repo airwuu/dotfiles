804dbeb9de9eb122765440dad91ccb01cf29b8d7
airwuu <awu0103@gmail.com>
2026-01-27 14:07:08 -0800

    style

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -14,6 +14,7 @@
         ../../home/system/neofetch
         ../../home/system/git
         ../../home/system/java
+	../../home/programs/waybar
 
         # Programs
         ../../home/programs/discord
